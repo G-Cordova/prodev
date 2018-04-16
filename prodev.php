6fa7709d8b3c392038c2466723f4ef0d00c99af4
G Cordova <gcordovaone@gmail.com>
2018-04-15 20:25:35 -0600

    created project and added necessary fields

---
+++ prodev.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>prodev</title>
+	</head>
+	<body>
+		<h1>G Cordova's Professional Development</h1>
+		Last Updated: April 15th, 2018
+
+		Career Buddy: Brent
+		<div>
+			<h2>Learning Style</h2>
+			Hands On
+			<h2>Quantitative Accomplishments</h2>
+			<ul>
+				<li>I built a 2-year plan for myself, along with a 5-year plan.</li>
+				<li>I started a 10-week immersive program toward my plans.</li>
+				<li>I added 1 baby to my family.</li>
+			</ul>
+			<h2>Qualitative Features</h2>
+			<ul>
+				<li>Free Thinker</li>
+				<li>Awareness/Consciousness</li>
+				<li>Unbias/Empathetic</li>
+			</ul>
+			<h2>Golden Circle</h2>
+			<ol>
+				<li>To bring happiness to others</li>
+				<li>I have no idea how I am going to do this yet.</li>
+				<li>I am in hopes that this will make our world better, even if it is just a little bit.</li>
+			</ol>
+			<h2>Elevator Pitch</h2>
+			<h3>Version 1</h3>
+			Good morning/afternoon/evening! My name is G Cordova. I am working on an app that is to help make this world better by bringing happiness to others. Let's grab lunch to discuss ideas!
+			<h2>Weekly Reflections</h2>
+			<h3>Week 1</h3>
+			I am feeling like I have a long way to get to where I am looking to get with my coding abilities. However, I knew that this would come with the territory when I started this. I have full faith in myself and my instructors that I will reach my goal through hard work and patience!
+		</div>
+	</body>
+</html>
